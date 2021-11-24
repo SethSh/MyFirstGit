40dc3023c98fc6a51c63f51e4b1472e9c1c97ed6
SethSh <sshenghit@icloud.com>
2021-11-24 17:42:15 -0500

    tests don't work

--- OnLevel.py
+++ OnLevel.py
@@ -1,40 +1,48 @@
-from numpy.core.fromnumeric import size
 import pandas as pd
 import datetime
-import time
-import random
-from pprint import pprint
-import numpy as np
-from pandas.core.frame import DataFrame
 
 class OnLevel:
+
     def Calculate(self, rateChanges, historicalPeriods, prospectivePeriod):
-        
-        def AppendPolicyFactors(rateChanges, policies):
-            if len(rateChanges) > 1:
-                for index in range(len(rateChanges)-1):
-                    lower = rateChanges['Date'].iloc[index]
-                    upper = rateChanges['Date'].iloc[index + 1]    
-                    f = rateChanges['CumulativeFactor'].iloc[index]
-                    policies.loc[(policies["Start"] >= lower) & (policies["Start"] < upper), "CumulativeFactor"] = f
-
-            if len(rateChanges) > 0:
-                policies.loc[policies["Start"] <  rateChanges["Date"][0], "CumulativeFactor"] = rateChanges["CumulativeFactor"][0]
-                policies.loc[policies["Start"] >= rateChanges["Date"].iloc[-1], "CumulativeFactor"] = rateChanges["CumulativeFactor"].iloc[-1]
-
-        
         unsortedRateChanges = rateChanges
-        unsortedRateChanges["Factor"] = unsortedRateChanges["Rate"] + 1
-        
-        rateChanges = unsortedRateChanges.groupby('Date').agg({'Factor': 'prod'}).sort_values(by="Date").reset_index()
-        rateChanges["CumulativeFactor"] = rateChanges["Factor"].iloc[::-1].cumprod()
+        unsortedRateChanges['Factor'] = unsortedRateChanges['Rate'] + 1
+        rateChanges = unsortedRateChanges.groupby('Date').agg({'Factor': 'prod'}).sort_values(by='Date').reset_index()
+        rateChanges['CumulativeFactor'] = rateChanges['Factor'].iloc[::-1].cumprod()
         
         earliestPolicyStartDate = historicalPeriods[0][0]
-        latestPolicyEndDate = prospectivePeriod[0][1]
+        latestPolicyEndDate = prospectivePeriod[1]
+        
         policies = pd.DataFrame(pd.date_range(start = earliestPolicyStartDate, end = latestPolicyEndDate), columns = ['Start'])
-        policies["PolicyEndDate"] = policies["Start"] + pd.DateOffset(years=1) + datetime.timedelta(days=-1)
-        policies["DayCount"] = policies["PolicyEndDate"] - policies["Start"] + datetime.timedelta(days=1)
-        policies["CumulativeFactor"] = 1
+        policies['End'] = policies['Start'] + pd.DateOffset(years=1) + datetime.timedelta(days=-1)
 
-        AppendPolicyFactors(rateChanges, policies)
+        policies.Start = policies.Start.apply(lambda x: x.date())
+        policies.End = policies.End.apply(lambda x: x.date())
+        
+        policies['DayCount'] = policies['End'] - policies['Start'] + datetime.timedelta(days=1)
+        self.__AppendPolicyCumulativeFactors(rateChanges, policies)
+
+        for period in historicalPeriods:
+            days = 0
+            weightedDays = 0
+            treatyStart, treatyEnd = period
+            print(treatyStart)
+            
+            subset = policies.loc[(policies.Start >= treatyStart) & (policies.End < treatyEnd)]
+            days += subset['DayCount'].sum()
+            weightedDays += (subset['DayCount'] * subset['CumulativeFactor']).sum()
+
         return 123;    
+
+
+    def __AppendPolicyCumulativeFactors(self, rateChanges, policies):
+        policies['CumulativeFactor'] = 1
+        if len(rateChanges) > 1:
+            for index in range(len(rateChanges)-1):
+                lower = rateChanges.Date.iloc[index]
+                upper = rateChanges.Date.iloc[index + 1]    
+                f = rateChanges['CumulativeFactor'].iloc[index]
+                policies.loc[(policies.Start >= lower) & (policies.Start < upper), 'CumulativeFactor'] = f
+
+        if len(rateChanges) > 0:
+            policies.loc[policies.Start <  rateChanges.Date[0], 'CumulativeFactor'] = rateChanges.CumulativeFactor[0]
+            policies.loc[policies.Start >= rateChanges.Date.iloc[-1], 'CumulativeFactor'] = rateChanges.CumulativeFactor.iloc[-1]
--- OnLevelTests.py
+++ OnLevelTests.py
@@ -1,73 +1,33 @@
 import unittest
-import datetime
 import pandas as pd
 import OnLevel as OnLevelClass
+import datetime
 
 class OnLevelTests(unittest.TestCase):
-    
-    # def test_upper(self):
-    #     self.assertEqual('foo'.upper(), 'FOO')
-        
+    historicalPeriods = [[datetime.date(year, 1, 1), datetime.date(year, 12, 31)] for year in range(2015, 2018)]
+    prospectivePeriod = [datetime.date(2022, 1, 1), datetime.date(2022, 12, 31)]
+
     def test_One_Rate_Change(self):
         onlevel = OnLevelClass.OnLevel()
         
-        count = 1
-        rateChanges = pd.DataFrame (data=
-                        {
-                            'Date' : ["2015-01-01"],
-                            'Rate' : [0.25]
-                        })
-        
-        historicalPeriods = []
-        for year in range(2015, 2018):
-            historicalPeriods.append( [datetime.datetime(year, 1, 1).date(), datetime.datetime(year, 12, 31).date()] )
-
-        prospectivePeriod = []
-        prospectivePeriod.append(["2022-01-01", "2022-12-31"])
-
-        result = onlevel.Calculate(rateChanges, historicalPeriods, prospectivePeriod)
+        rateChanges = pd.DataFrame (data= {'Date' : [datetime.date(2015,1,1)], 'Rate' : [0.25] })
+        result = onlevel.Calculate(rateChanges, self.historicalPeriods, self.prospectivePeriod)
         self.assertEqual(result, 123)
 
     
-    def test_Two_Rate_Changes(self):
+    def Test_Two_Rate_Changes(self):
         onlevel = OnLevelClass.OnLevel()
         
-        count = 2
-        rateChanges = pd.DataFrame (data=
-                        {
-                            'Date' : ["2015-01-01", "2015-07-01"],
-                            'Rate' : [0.25, 0.1]
-                        })
-            
-        historicalPeriods = []
-        for year in range(2015, 2018):
-            historicalPeriods.append( [datetime.datetime(year, 1, 1).date(), datetime.datetime(year, 12, 31).date()] )
-
-        prospectivePeriod = []
-        prospectivePeriod.append(["2022-01-01", "2022-12-31"])
-        
-        result = onlevel.Calculate(rateChanges, historicalPeriods, prospectivePeriod)
+        rateChanges = pd.DataFrame (data= {'Date' : [datetime.date(2015,1,1), datetime.datetime.date(2015,7,1)],'Rate' : [0.25, 0.1] })
+        result = onlevel.Calculate(rateChanges, self.historicalPeriods, self.prospectivePeriod)
         self.assertEqual(result, 123)
 
 
-    def test_Two_Rate_Changes_With_Duplicate(self):
+    def Test_Two_Rate_Changes_With_Duplicate(self):
         onlevel = OnLevelClass.OnLevel()
         
-        count = 2
-        rateChanges = pd.DataFrame (data=
-                        {
-                            'Date' : ["2015-01-01", "2015-07-01", "2015-07-01"],
-                            'Rate' : [0.25, 0.1, 0.15]
-                        })
-            
-        historicalPeriods = []
-        for year in range(2015, 2018):
-            historicalPeriods.append( [datetime.datetime(year, 1, 1).date(), datetime.datetime(year, 12, 31).date()] )
-
-        prospectivePeriod = []
-        prospectivePeriod.append(["2022-01-01", "2022-12-31"])
-        
-        result = onlevel.Calculate(rateChanges, historicalPeriods, prospectivePeriod)
+        rateChanges = pd.DataFrame (data= {'Date' : [datetime.date(2015,1,1), datetime.date(2015,7,1), datetime.date(2015,7,1)], 'Rate' : [0.25, 0.1, 0.15] })            
+        result = onlevel.Calculate(rateChanges, self.historicalPeriods, self.prospectivePeriod)
         self.assertEqual(result, 123)
 
         
