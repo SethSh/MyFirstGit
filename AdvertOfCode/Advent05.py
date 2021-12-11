b09ec5ebb472fe8ceba6eb67f4f7f21a4726f673
SethSh <sshenghit@icloud.com>
2021-12-11 18:40:22 -0500

    working on 5

--- AdvertOfCode/Advent05.py
+++ AdvertOfCode/Advent05.py
@@ -1,15 +1,21 @@
 import numpy as np
 
-filename = 'Day5Data.txt'
-data = np.loadtxt(filename, delimiter='->', skiprows=0, dtype=str).tolist()
+fileName = 'Day5Data.txt'
+data = np.loadtxt(fileName, delimiter='->', skiprows=0, dtype=str).tolist()
 
 row_count = len(data)
 length = len(data[0])
 print(f"rows {row_count} length {length}")
 
+counter = 0
 for row_item in data:
-    res = [int(i.strip().replace(',','')) for i in row_item]
-    print(res)
-# print(data[0][0])
-# print(data[0][1])
-# print(type(data[0]))
+    res = [i.split(',') for i in row_item]
+    if counter == 0:
+        first = [int(i) for i in res[0]]
+        x1, y1 = first
+        print(f"x1 {x1} and y1 {y1}") 
+
+        second = [int(i) for i in res[1]]
+        x2, y2 = second
+        print(f"x2 {x2} and y2 {y2}") 
+        counter +=1
