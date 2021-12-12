4e32721bce01038cfff213c466485bc48380924f
SethSh <sshenghit@icloud.com>
2021-12-11 20:36:29 -0500

    more day 5

--- AdvertOfCode/Advent05.py
+++ AdvertOfCode/Advent05.py
@@ -7,15 +7,38 @@ row_count = len(data)
 length = len(data[0])
 print(f"rows {row_count} length {length}")
 
+dimensionSize = 1000
+countArray  = np.zeros(dimensionSize * dimensionSize)
+
+countArray = []
+for i in range(0,dimensionSize):
+    countArray.append(np.zeros(dimensionSize))
+
 counter = 0
 for row_item in data:
     res = [i.split(',') for i in row_item]
-    if counter == 0:
-        first = [int(i) for i in res[0]]
-        x1, y1 = first
-        print(f"x1 {x1} and y1 {y1}") 
+    
+    first = [int(i) for i in res[0]]
+    x1, y1 = first
+    
+    second = [int(i) for i in res[1]]
+    x2, y2 = second
+    #print(f"x1 {x1} and y1 {y1} AND x2 {x2} and y2 {y2}")     
 
-        second = [int(i) for i in res[1]]
-        x2, y2 = second
-        print(f"x2 {x2} and y2 {y2}") 
-        counter +=1
+    if x1 == x2:
+        miniumumValue = min(y1, y2)
+        maximumValue = max(y1, y2)
+        for i in range(miniumumValue, maximumValue+1):
+            countArray[x1][i] += 1
+    else:
+        miniumumValue = min(x1, x2)
+        maximumValue = max(x1, x2)
+        for i in range(miniumumValue, maximumValue+1):
+            countArray[i][y1] += 1
+
+total = 0
+for i in range(0,dimensionSize):
+    total += sum(countArray[i] >= 2)
+print(total)
+
+
