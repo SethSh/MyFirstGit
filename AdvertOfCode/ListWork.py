e1a28df8b197b0db002fe31cdab9cfa606a44ab8
Seth Shenghit <sshenghit@icloud.com.com>
2021-12-17 20:03:05 -0500

    day 14

---
+++ AdvertOfCode/ListWork.py
@@ -0,0 +1,15 @@
+import numpy as np
+array = np.arange(5)
+print(array)
+
+print(type(array))
+
+array[array > 3] = 3
+print(array)
+
+list = [1, 2, 'seth']
+print (list)
+
+arr2 = np.array([1, 'seth'])
+print(arr2)
+
